5dd503ef065cdc6258ec94eb9241524c91320dda
Parker <parkerproject@gmail.com>
2016-02-19 00:33:29 -0500

    updates

--- starter.sh
+++ starter.sh
@@ -4,5 +4,5 @@ if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 
 then
   export PATH=/usr/local/bin:$PATH
   export NODE_ENV=production
-  cd /var/www/placeful-api && pm2 start server.js -i 0 >> forever.log 2>&1
+  cd /var/www/placeful-api && pm2 start app.js -i 0 >> forever.log 2>&1
 fi
