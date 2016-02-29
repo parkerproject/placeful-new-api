8cb0c11adb74adf741bf41953596f41b5aea518e
Parker <parkerproject@gmail.com>
2016-02-29 16:07:27 -0500

    added places for follower

--- routes.js
+++ routes.js
@@ -1,49 +1,53 @@
 var requireDirectory = require('require-directory')
 
 module.exports = function (server) {
-  var controller = requireDirectory(module, './controllers')
+    var controller = requireDirectory(module, './controllers')
 
-  // Array of routes for Hapi
-  var routeTable = [{
-    method: 'GET',
-    path: '/images/{path*}',
-    config: controller.assets.images
+    // Array of routes for Hapi
+    var routeTable = [{
+        method: 'GET',
+        path: '/images/{path*}',
+        config: controller.assets.images
   }, {
-    method: 'GET',
-    path: '/css/{path*}',
-    config: controller.assets.css
+        method: 'GET',
+        path: '/css/{path*}',
+        config: controller.assets.css
   }, {
-    method: 'GET',
-    path: '/js/{path*}',
-    config: controller.assets.js
+        method: 'GET',
+        path: '/js/{path*}',
+        config: controller.assets.js
   }, {
-    method: 'GET',
-    path: '/video/{path*}',
-    config: controller.assets.video
+        method: 'GET',
+        path: '/video/{path*}',
+        config: controller.assets.video
   }, {
-    method: 'GET',
-    path: '/promotions',
-    config: controller.promotions.index
+        method: 'GET',
+        path: '/promotions',
+        config: controller.promotions.index
   }, {
-    method: 'GET',
-    path: '/promotion',
-    config: controller.promotion.index
+        method: 'GET',
+        path: '/promotion',
+        config: controller.promotion.index
   }, {
-    method: 'GET',
-    path: '/places',
-    config: controller.places.index
+        method: 'GET',
+        path: '/places',
+        config: controller.places.index
   }, {
-    method: 'GET',
-    path: '/place',
-    config: controller.place.index
+        method: 'GET',
+        path: '/places/follower',
+        config: controller.places_follower.index
   }, {
-    method: 'PUT',
-    path: '/place/follow',
-    config: controller.follow.index
+        method: 'GET',
+        path: '/place',
+        config: controller.place.index
   }, {
-    method: 'DELETE',
-    path: '/place/unfollow',
-    config: controller.unfollow.index
+        method: 'PUT',
+        path: '/place/follow',
+        config: controller.follow.index
+  }, {
+        method: 'DELETE',
+        path: '/place/unfollow',
+        config: controller.unfollow.index
   }]
-  return routeTable
+    return routeTable
 }
