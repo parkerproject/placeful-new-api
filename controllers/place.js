36b3e8a0462f56a4e4de265ef4ce9fd7924aeca4
Parker <parkerproject@gmail.com>
2016-03-18 14:04:37 -0400

    added large image

--- controllers/place.js
+++ controllers/place.js
@@ -1,5 +1,5 @@
 require('dotenv').load()
-const collections = ['merchants']
+const collections = ['merchants', 'promotions']
 const mongojs = require('mongojs')
 const db = mongojs.connect(process.env.MONGODB_URL, collections)
 const Joi = require('joi')
@@ -10,16 +10,20 @@ module.exports = {
     handler: function (request, reply) {
       'use strict'
 
-      if (!request.query.key || request.query.key != process.env.API_KEY) {
+      if (!request.query.key || request.query.key !== process.env.API_KEY) {
         reply('You need an api key to access data')
       }
 
       db.merchants.find({
         business_id: request.query.business_id
       }).limit(1, function (err, result) {
-        reply(result)
+        if (err) console.log(err)
+        db.promotions.find({}).limit(1, (err, promos) => {
+          if (err) console.log(err)
+          result[0].large_image = promos[0].large_image
+          reply(result)
+        })
       })
-
     },
 
     description: 'View a place',
