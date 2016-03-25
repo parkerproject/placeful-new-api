2fc1e6e7b313f602a6e9fc865efab9d6080e8a57
Parker <parkerproject@gmail.com>
2016-03-25 14:00:27 -0400

    fixed notifications

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -3,47 +3,56 @@ const collections = ['notifications']
 const mongojs = require('mongojs')
 const db = mongojs.connect(process.env.MONGODB_URL, collections)
 const Joi = require('joi')
-
 module.exports = {
   index: {
     handler: function (request, reply) {
       'use strict'
-
       if (!request.query.key || request.query.key !== process.env.API_KEY) {
         reply('You need an api key to access data')
       }
-
       let skip = request.query.offset || 0
       let limit = request.query.limit || 20
-      let count = 0
-
-      db.notifications.count({}, (err, res) => {
-        if (err) console.log(err)
-        count = res
-        db.notifications.find({}).sort({_id: -1}).skip(skip).limit(limit, (err, results) => {
+      if (request.query.notification_id) {
+        db.notifications.find({
+          item_id: request.query.notification_id
+        }, (err, result) => {
+          if (err) console.log(err)
+          let createdDateOfNotification = result[0].date_created
+          db.notifications.find({
+            date_created: {
+              $gt: createdDateOfNotification
+            }
+          }, (err, data) => {
+            if (err) console.log(err)
+            reply({
+              results: data,
+              total_amount: data.length
+            })
+          })
+        })
+      } else {
+        db.notifications.find({}).sort({
+          _id: -1
+        }).skip(skip).limit(limit, (err, results) => {
           if (err) console.log(err)
           reply({
             results: results,
-            total_amount: count
+            total_amount: results.length
           })
         })
-      })
+      }
     },
-
     description: 'get notifications',
     notes: 'get notifications',
     tags: ['api'],
-
     validate: {
       query: {
         key: Joi.string().required().description('API key to access data'),
         user_id: Joi.string().required().description('id of user'),
         limit: Joi.number().integer().min(1).default(20).description('defaults to 20'),
         offset: Joi.number().integer().description('defaults to 0'),
-        notification_id: Joi.string().description('notification id')
+        notification_id: Joi.string().description('item_id of the notification')
       }
     }
-
   }
-
 }
