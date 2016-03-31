46cfc43246eed8ce053750f002c6ed4a93f90866
Parker <parkerproject@gmail.com>
2016-03-31 15:07:56 -0400

    tested

--- controllers/email.js
+++ controllers/email.js
@@ -3,34 +3,30 @@ require('dotenv').load()
 const sendEmail = require('./send_email')
 const swig = require('swig')
 const Joi = require('joi')
-
 module.exports = {
   welcome: {
     handler: (request, reply) => {
       if (!request.payload.key || request.payload.key !== process.env.API_KEY) {
         reply('You need an api key to access data')
       }
-
       let subject = 'Welcome to Placeful'
       let email = request.payload.email
-
       swig.renderFile(appRoot + '/views/welcome_email.html', {
         name: request.payload.name,
         email: email
-      },
-        function (err, content) {
-          if (err) {
-            throw err
-          }
-          sendEmail(email, subject, content)
-          reply({message: 'Email sent'})
+      }, function (err, content) {
+        if (err) {
+          throw err
+        }
+        sendEmail(email, subject, content)
+        reply({
+          message: 'Email sent'
         })
-
+      })
     },
     description: 'welcome email',
     notes: 'welcome email',
     tags: ['api'],
-
     validate: {
       payload: {
         key: Joi.string().required().description('API key to access data'),
@@ -39,32 +35,29 @@ module.exports = {
       }
     }
   },
-
   ticket: {
     handler: (request, reply) => {
       if (!request.payload.key || request.payload.key !== process.env.API_KEY) {
         reply('You need an api key to access data')
       }
-
       let subject = 'You joined a ticket'
       let email = request.payload.email
-
+      console.log(request.payload)
       swig.renderFile(appRoot + '/views/ticket.html', {
         data: request.payload
-      },
-        function (err, content) {
-          if (err) {
-            throw err
-          }
-          sendEmail(email, subject, content)
-          reply({message: 'Email sent'})
+      }, function (err, content) {
+        if (err) {
+          throw err
+        }
+        sendEmail(email, subject, content)
+        reply({
+          message: 'Email sent'
         })
-
+      })
     },
     description: 'ticket email',
     notes: 'ticket email',
     tags: ['api'],
-
     validate: {
       payload: {
         key: Joi.string().required().description('API key to access data'),
