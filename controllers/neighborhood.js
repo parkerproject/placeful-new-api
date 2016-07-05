7b2cdf508363d59c73b11eb7047237e2dd8cd038
Parker <parkerproject@gmail.com>
2016-07-05 15:46:36 -0400

    neighbor

---
+++ controllers/neighborhood.js
@@ -0,0 +1,35 @@
+module.exports = function () {
+  var areas = [
+    'All',
+    'Astoria',
+    'Brooklyn',
+    'Chelsea',
+    'East Village',
+    'Financial District',
+    'Flatiron',
+    'Gramercy',
+    'Greenwich Village',
+    'Harlem',
+    "Hell's Kitchen",
+    'Kips Bay',
+    'Lower East Side',
+    'Meatpacking District',
+    'Midtown East',
+    'Midtown West',
+    'Murray Hill',
+    'NoHo',
+    'Nolita',
+    'Park Slope',
+    'Queens',
+    'SoHo',
+    'Theater District',
+    'TriBeCa',
+    'Union Square',
+    'Upper East Side',
+    'Upper West Side',
+    'West Village',
+    'Williamsburg'
+  ]
+
+  return areas
+}
