64d85b3e7003b3104119142249ede0d1d946d6b6
Parker <parkerproject@gmail.com>
2017-05-26 08:18:37 -0400

    updates

--- controllers/user_guides.js
+++ controllers/user_guides.js
@@ -1,11 +1,29 @@
+const paginate = require('mongojs-paginate');
 const db = require('../helpers/db');
 
 
 module.exports = {
   index: {
     handler(request, reply) {
-      const queryObject = { user: request.query.user };
-      db.guides.find(queryObject, (err, results) => {
+      const page = request.query.page || 1;
+      const limit = request.query.limit || 20;
+
+      const query = db.guides.find({ user: request.query.user });
+
+      paginate(query, {
+        limit,
+        page,
+      }, (err, results) => {
+        // Result:
+        // items: containing items of the desired page
+        // itemCount: non paged count of items returned by query
+        // page: current page
+        // pageCount: Number of pages
+        // limit: Number of items returned by page
+        // next: index of the next page or undefined if no next page exists
+        // hasNext: true if next page exists otherwise false
+        // previous: index of the previous page or undefined if no previous page exists
+        // hasPrevious: true if previous page exists otherwise false
         reply(results);
       });
     },
